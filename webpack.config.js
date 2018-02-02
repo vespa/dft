9d4e4e5e7bd3d4ce35d4815813e217a505dbbc55
Danilo Vespa <d.vespa@gmail.com>
2018-02-02 11:33:44 -0200

    initial project config

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+module.exports = {
+  entry: ["./src/js/app.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  devServer: {
+    contentBase: "./dist",
+    historyApiFallback: true,
+  },
+  resolve:{
+      alias:{
+        container: path.resolve(__dirname, "src/js/components/container/"),
+        presentational: path.resolve(__dirname, "src/js/components/presentational/"),
+        config: path.resolve(__dirname, "src/js/components/config/"),
+        libs: path.resolve(__dirname, "src/js/libs/"),
+      }
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.json$/, 
+        loader: "file-loader",
+        options: {
+          name: '[path][name].[ext]?[hash]',
+        }   
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+      }, {
+          loader: "css-loader"
+      }, {
+          loader: "sass-loader",
+          options: {
+              includePaths: ["absolute/path/a", "absolute/path/b"]
+          }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    //new UglifyJsPlugin()
+  ]
+};
