fae1c014193034e43f77ff69b18a387a944549c7
Danilo Vespa <d.vespa@gmail.com>
2018-02-05 13:20:02 -0200

    putting the add address form in a modal

--- src/js/components/container/formElements/FormAddNewAddress.js
+++ src/js/components/container/formElements/FormAddNewAddress.js
@@ -59,104 +59,116 @@ class FormAddNewAddress extends Component {
     const {address, number, complement, type, zip_code, observations, billing_address} = this.state;
     const obj = {address, number, complement, type, zip_code, observations, billing_address}
     this.props.updateAddressList(obj);
-    this.switchVisibility();
-
   }
   
   tryUpdate(e){
     e.preventDefault();
     this.updateList();
+    $('#modalAddAddress').modal('toggle');
   }
-
+  formContent(){
+    return 
+  }
   render() {
     const {value, buttonVisiblity} = this.state;
     const buttonStyle = {
-      display: (buttonVisiblity)? "":"none",
       cursor: "pointer"
-    };
-    const formStyle = {
-      display: (!buttonVisiblity)? "":"none"
     };
     return (
       <form className="needs-validation" onSubmit={this.tryUpdate}>
-        <div  style={formStyle} > 
-          <div className="row form-group">
-            <div className="col-sm-4">
-              <label className="field__label">Tipo</label>
-              <FormElementSelect 
-                  options={AddressTypeOptions}  
-                  onChange={this.setStateValue("type")}
-              /> 
-            </div>
-            <div className="col">
-            <label className="field__label">Logradouro</label>
-              <input 
-                type="text" 
-                onChange={this.setStateValue("address")}
-                value={this.state.address}
-                className="form-control"
-                required
-                 />
-            </div>
-          </div>
-          <div className="row form-group">
-            <div className="col-sm">
-              <label className="field__label">Número</label>
-              <input 
-                type="text" 
-                onChange={this.setStateValue("number")}
-                value={this.state.number}
-                className="form-control"
-                required
-                 />
-            </div>
-            <div className="col-sm">
-                <label className="field__label">Complemento</label>
-                <input 
-                  type="text" 
-                    onChange={this.setStateValue("complement")}
-                    value={this.state.complement}
-                    className="form-control"
-                    required
-                   />
-            </div>
-          </div>
-          <div className="row form-group">
-            <div className="col">
-                <label className="field__label">CEP</label>
-                <input 
-                  type="text" 
-                    onChange={this.setStateValue("zip_code")}
-                    value={this.state.zip_code}
-                    className="form-control"
-                    required
-                   />
-            </div>
-            <div className="col-sm-6">
-                <label className="field__label">Endereço de cobrança</label>
-                    <FormElementSelect 
-                        options={BillingOptions}  
-                        onChange={this.setStateValue("billing_address", Boolean)}
-                    /> 
+         <div>
+            <div className="modal fade bd-example-modal-lg" id="modalAddAddress" tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="h">Adicionar endereço</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                   <div className="row form-group">
+                    <div className="col-sm-4">
+                      <label className="field__label">Tipo</label>
+                      <FormElementSelect 
+                          options={AddressTypeOptions}  
+                          onChange={this.setStateValue("type")}
+                      /> 
+                    </div>
+                    <div className="col">
+                    <label className="field__label">Logradouro</label>
+                      <input 
+                        type="text" 
+                        onChange={this.setStateValue("address")}
+                        value={this.state.address}
+                        className="form-control"
+                        required
+                         />
+                    </div>
+                  </div>
+                  <div className="row form-group">
+                    <div className="col-sm">
+                      <label className="field__label">Número</label>
+                      <input 
+                        type="text" 
+                        onChange={this.setStateValue("number")}
+                        value={this.state.number}
+                        className="form-control"
+                        required
+                         />
+                    </div>
+                    <div className="col-sm">
+                        <label className="field__label">Complemento</label>
+                        <input 
+                          type="text" 
+                            onChange={this.setStateValue("complement")}
+                            value={this.state.complement}
+                            className="form-control"
+                            required
+                           />
+                    </div>
+                  </div>
+                  <div className="row form-group">
+                    <div className="col">
+                        <label className="field__label">CEP</label>
+                        <input 
+                          type="text" 
+                            onChange={this.setStateValue("zip_code")}
+                            value={this.state.zip_code}
+                            className="form-control"
+                            required
+                           />
+                    </div>
+                    <div className="col-sm-6">
+                        <label className="field__label">Endereço de cobrança</label>
+                            <FormElementSelect 
+                                options={BillingOptions}  
+                                onChange={this.setStateValue("billing_address", Boolean)}
+                            /> 
+                      </div>
+                  </div>
+                  <div className="row form-group">
+                      <div className="col-sm">
+                        <label className="field__label">Observações</label>
+                          <textarea 
+                          type="text" 
+                          onChange={this.setStateValue("observations")}
+                          value={this.state.observations}
+                          className="form-control"
+                         />
+
+                    </div>
+                  </div>
+                   </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancela</button>
+                    <button type="button" className="btn btn-primary" type="submit">Confirma</button>
+                  </div>
+                </div>
               </div>
-          </div>
-          <div className="row form-group">
-              <div className="col-sm-6">
-                <label className="field__label">Observações</label>
-                  <textarea 
-                  type="text" 
-                  onChange={this.setStateValue("observations")}
-                  value={this.state.observations}
-                  className="form-control"
-                 />
-
             </div>
-          </div>
-
-          <button  type="submit"  className="btn btn-primary margin-h"> adiciona </button>
-          <a  onClick={this.switchVisibility} className="btn btn-danger margin-h" role="button" href="#"> cancela </a>
         </div>
-        <a style={buttonStyle} href="#" role="button" onClick={this.switchVisibility} className="btn btn-primary"> {value} </a>
+        <a style={buttonStyle} href="#" role="button" data-toggle="modal" data-target="#modalAddAddress"   className="btn btn-primary"> {value} </a>
 
       </form>
     );
@@ -171,4 +183,6 @@ FormAddNewAddress.propTypes = {
     ]).isRequired,
     updateAddressList: PropTypes.func.isRequired
 }
+
+
 
